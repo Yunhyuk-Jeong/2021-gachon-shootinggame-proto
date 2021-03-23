1da8b60ae64ef0c230b7a8bd452ce64ca0286c2a
정윤혁 <yunhyukjeong02@gmail.com>
2021-03-23 19:37:19 +0900

    prototype

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5;
+
+    Vector3 dir;
+
+    void Start()
+    {
+        int randValue = UnityEngine.Random.Range(0, 10);
+
+        if (randValue < 3)
+        {
+            GameObject target = GameObject.Find("Player");
+            dir = target.transform.position - transform.position;
+            dir.Normalize();
+        }
+        else
+        {
+            dir = Vector3.down;
+        }
+    }
+
+    void Update()
+    {
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
